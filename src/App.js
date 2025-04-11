5fbf6e2c9c844afe31e568bb9d74ce3042790465
Sivasha787 <sivasha787@gmail.com>
2025-04-11 11:17:09 +0530

    initial commit and project setup

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Create portfolio in React 
         </a>
       </header>
     </div>
